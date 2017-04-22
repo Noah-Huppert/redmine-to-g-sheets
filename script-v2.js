fad422c0ce06e5c43795391eb4348d8a719a1822
Noah Huppert <noahhuppert@gmail.com>
2017-04-22 16:22:33 -0400

    Issue sorting

--- script-v2.js
+++ script-v2.js
@@ -10,36 +10,36 @@ var RECURSION_MAX_DEFAULT = 20;
  * @returns {{}} Deep copy of obj
  */
 function deepCopy(obj, recursionCounter, recursionMax) {
-    // Recursion
-    if (recursionCounter === undefined) {
-        recursionCounter = 0;
-    } else {
-        recursionCounter += 1;
+  // Recursion
+  if (recursionCounter === undefined) {
+    recursionCounter = 0;
+  } else {
+    recursionCounter += 1;
+  }
+
+  if (recursionMax === undefined) {
+    recursionMax = RECURSION_MAX_DEFAULT;
+  } else if (recursionCounter > recursionMax) {
+    throw Error("Reached max recursion depth: " + recursionMax);
+  }
+
+  // Copy
+  var newCopy = {};
+
+  // For each key
+  for (var key in obj) {
+    // If object
+    if (typeof obj[key] === Object) {
+      // Recursion
+      newCopy[key] = deepCopy(obj[key], recursionCounter, recursionMax);
+    } else {// If regular value
+      // Copy normally
+      newCopy[key] = obj[key];
     }
+  }
 
-    if (recursionMax === undefined) {
-        recursionMax = RECURSION_MAX_DEFAULT;
-    } else if (recursionCounter > recursionMax) {
-        throw Error("Reached max recursion depth: " + recursionMax);
-    }
-
-    // Copy
-    var newCopy = {};
-
-    // For each key
-    for (var key in obj) {
-        // If object
-        if (typeof obj[key] === Object) {
-            // Recursion
-            newCopy[key] = deepCopy(obj[key], recursionCounter, recursionMax);
-        } else {// If regular value
-            // Copy normally
-            newCopy[key] = obj[key];
-        }
-    }
-
-    // Return copy
-    return newCopy;
+  // Return copy
+  return newCopy;
 }
 
 // Issue Class
@@ -47,95 +47,129 @@ function deepCopy(obj, recursionCounter, recursionMax) {
  * Class which represents Redmine issue loaded in from spreadsheet.
  * @param id Id
  * @param type Type, either bug or task
- * @param parendId Parent Id or undefined if parent
+ * @param parentId Parent Id or undefined if parent
  * @param status Current status of issue
  * @param priority How important issue is
  * @param subject Short summary of issue
  * @param assignee Team member assigned to issue
+ * @param updated Last Date issue was updatedd
  * @param startDate Task start date
  * @param dueDate Task due date
  * @param estimatedTime Estimated issue completion time
  * @param percentDone Issue Completion
+ * @param created Date Issue was created
+ * @param closed If issue is closed
+ * @param rawRelatedIssues Text describing related Issues
  * @returns {Issue}
  * @constructor
  */
-function Issue(id, type, parentId, status, priority, subject, assignee, startDate, dueDate, estimatedTime, percentDone) {
-    // Constructor
-    this.id = id;
-    this.type = type;
-    this.parentId = parentId.substr(1);
-    this.status = status;
-    this.priority = priority;
-    this.subject = subject;
-    this.assignee = assignee;
-    this.startDate = startDate;
-    this.dueDate = dueDate;
-    this.estimatedTime = estimatedTime;
-    this.percentDone = percentDone;
+function Issue(id,
+               type,
+               parentId,
+               status,
+               priority,
+               subject,
+               assignee,
+               updated,
+               startDate,
+               dueDate,
+               estimatedTime,
+               percentDone,
+               created,
+               closed,
+               rawRelatedIssues) {
 
-    /**
-     * Determines if Issue has parent issue
-     * @returns {boolean}
-     */
-    this.hasParent = function () {
-        return this.parentId !== undefined && this.parentId.length > 0;
-    };
+  // Constructor
+  this.id = id;
+  this.type = type;
+  this.parentId = parentId.substr(1);
+  this.status = status;
+  this.priority = priority;
+  this.subject = subject;
+  this.assignee = assignee;
+  this.updated = updated;
+  this.startDate = startDate;
+  this.dueDate = dueDate;
+  this.estimatedTime = estimatedTime;
+  this.percentDone = percentDone;
+  this.created = created;
+  this.closed = closed;
+  this.rawRelatedIssues = rawRelatedIssues;
 
-    /**
-     * Convert Issue into Row Array to be returned from spreadsheet function
-     * @returns {Array}
-     */
-    this.toRowArray = function () {
-        return [
-            [
-                "#",
-                "Parent #",
-                "Type",
-                "Priority",
-                "Status",
-                "Subject",
-                "Assignee",
-                "Start date",
-                "Due date",
-                "Estimated time",
-                "Percent done"
-            ],
-            [
-                this.id,
-                this.parentId,
-                this.type,
-                this.priority,
-                this.status,
-                this.subject,
-                this.assignee,
-                this.startDate,
-                this.dueDate,
-                this.estimatedTime,
-                this.percentDone
-            ]
-        ];
-    };
+  this.children = [];
 
-    return this;
+  /**
+   * Determines if Issue has parent issue
+   * @returns {boolean}
+   */
+  this.hasParent = function () {
+    return this.parentId !== undefined && this.parentId.length > 0;
+  };
+
+  /**
+   * Convert Issue into Row Array to be returned from spreadsheet function
+   * @returns {Array}
+   */
+  this.toRowArray = function () {
+    return [
+      [
+        "#",
+        "Parent #",
+        "Type",
+        "Priority",
+        "Status",
+        "Subject",
+        "Assignee",
+        "Start date",
+        "Due date",
+        "Estimated time",
+        "Percent done"
+      ],
+      [
+        this.id,
+        this.parentId,
+        this.type,
+        this.priority,
+        this.status,
+        this.subject,
+        this.assignee,
+        this.startDate,
+        this.dueDate,
+        this.estimatedTime,
+        this.percentDone
+      ]
+    ];
+  };
+
+  return this;
 }
 
 // Source Issues Sheet Colums
 Issue.COLUMNS = {
-    id: 0,
-    tracker: 2,
-    parentId: 3,
-    status: 4,
-    priority: 5,
-    subject: 6,
-    assignee: 8,
-    updated: 9,
-    startDate: 12,
-    dueDate: 13,
-    estimatedTime: 14,
-    percentDone: 15,
-    created: 16,
-    closed: 17,
-    relatedIssues: 18
+  id: 0,
+  tracker: 2,
+  parentId: 3,
+  status: 4,
+  priority: 5,
+  subject: 6,
+  assignee: 8,
+  updated: 9,
+  startDate: 12,
+  dueDate: 13,
+  estimatedTime: 14,
+  percentDone: 15,
+  created: 16,
+  closed: 17,
+  relatedIssues: 18
+};
+
+// Priority column values
+Issue.PRIORITY_SORT_VALUES = {
+  "Low": 0,
+  "Normal": 1,
+  "High": 2,
+  "Urgent": 3,
+  "Immediate": 4
 };
 
 // factories
@@ -145,118 +179,178 @@ Issue.COLUMNS = {
  * @returns Issue
  */
 Issue.fromRow = function (row) {
-    var args = [];
+  var args = [];
 
-    for (var key in Issue.COLUMNS) {
-        var col = Issue.COLUMNS[key];
-        args.push(row[col]);
-    }
+  for (var key in Issue.COLUMNS) {
+    var col = Issue.COLUMNS[key];
+    args.push(row[col]);
+  }
 
-    return Issue.apply(null, args);
+  return Issue.apply(null, args);
 };
 
 /**
  * Returns array of Issues from Source Issues Sheet rows
  * @param rows Rows of Issues
- * @returns Map of Issues
+ * @returns {Issue} Map of Issues
  */
 Issue.fromRows = function (rows) {
-    var issues = {};
+  var issues = {};
 
-    for (var i = 0; i < rows.length; i++) {
-        var issue = Issue.fromRow(rows[i]);
-        // Check that we aren't reading a column header row
-        if (issue.id !== "#") {
-            issues[issue.id] = issue;
-        }
+  for (var i = 0; i < rows.length; i++) {
+    var issue = Issue.fromRow(rows[i]);
+    // Check that we aren't reading a column header row
+    if (issue.id !== "#") {
+      issues[issue.id] = deepCopy(issue);
     }
+  }
 
-    return issues;
+  return issues;
+};
+
+// compare
+/**
+ * Compares two issues based on their start date, if their start dates are equal then compares via due date. If both
+ * dates are equal priority is used.
+ * @param a First issue
+ * @param b Second issue
+ * @returns -1 if a is less than b, 1 is a is greater than b, 0 if equal
+ */
+Issue.compareIssues = function (a, b) {
+  // First compare by start date
+  if (a.startDate < b.startDate) {
+    return -1;
+  } else if (a.startDate > b.startDate) {
+    return 1;
+  }
+
+  // If start dates are equal compare by due date
+  if (a.dueDate < b.dueDate) {
+    return -1;
+  } else if (a.dueDate > b.dueDate) {
+    return 1;
+  }
+
+  // If dates are equal use priority
+  var aP = Issue.PRIORITY_SORT_VALUES[a.priority];
+  var bP = Issue.PRIORITY_SORT_VALUES[b.priority];
+
+  if (aP < bP) {
+    return -1;
+  } else if (aP > bP) {
+    return 1;
+  }
+
+
+  // If both dates and priority are equal
+  return 0;
 };
 
 // IssueTree Class
 /**
  * Class which stores Issue class tree, used to represent parent and child tasks.
  * Simplified to deal with only Redmine issues 1 level deep.
+ *
+ * Consider immutable because sorting and parent issue mapping only occurs in constructor.
  * @param issues Issues map to construct tree out of
  * @returns {IssueTree}
  * @constructor
  */
 function IssueTree(issues) {
-    this.parentIssues = {};
+  this.parentIssues = {};
 
-    // For each issue
-    for (var id in issues) {
-        var issue = issues[id];
+  // Place issues and subissues
+  // For each issue
+  for (var id in issues) {
+    var issue = issues[id];
+    Logger.info(issue.id);
 
-        // If is parent and no parent issue placeholder
-        if (issue.hasParent() === false && (issue.id in this.parentIssues) === false) {
-            this.parentIssues[issue.id] = {
-                issue: issue,
-                children: {}
-            };
-        } else if (issue.hasParent() === false && (issue.id in this.parentIssues) === true) {// If is parent and parent issue placeholder exists
-            this.parentIssues[issue.id].issue = issue;
-        } else if (issue.hasParent() === true && (issue.parentId in this.parentIssues) === true) {// If has parent and parent exists
-            this.parentIssues[issue.parentId].children[issue.id] = issue;
-        } else if (issue.hasParent() === true && (issue.parentId in this.parentIssues) === false) {// If has parent and parent doesn't exist yet
-            var childrenMap = {};
-            childrenMap[issue.id] = issue;
+    // If is parent and no parent issue placeholder
+    if (issue.hasParent() === false && (issue.id in this.parentIssues) === false) {
+      this.parentIssues[issue.id] = {
+        issue: issue,
+        children: {}
+      };
+    } else if (issue.hasParent() === false && (issue.id in this.parentIssues) === true) {// If is parent and parent issue placeholder exists
+      this.parentIssues[issue.id].issue = issue;
+    } else if (issue.hasParent() === true && (issue.parentId in this.parentIssues) === true) {// If has parent and parent exists
+      this.parentIssues[issue.parentId].children[issue.id] = issue;
+    } else if (issue.hasParent() === true && (issue.parentId in this.parentIssues) === false) {// If has parent and parent doesn't exist yet
+      var childrenMap = {};
+      childrenMap[issue.id] = issue;
 
-            this.parentIssues[issue.parentId] = {
-                issue: undefined,
-                children: childrenMap
-            };
-        }
+      this.parentIssues[issue.parentId] = {
+        issue: undefined,
+        children: childrenMap
+      };
     }
 
-    /**
-     * Convert IssueTree into Row Array to be returned from spreadsheet function
-     * @returns {Array}
-     */
-    this.toRowArray = function () {
-        var out = [];
+    issue = undefined;
+  }
 
+  // Sort parent issues
+  this.sortedParentIssues = [];
 
-        // For each parent
-        for (var pId in this.parentIssues) {
-            // Add parent row
-            var parent = this.parentIssues[pId];
+  // For each parent issue
+  for (var pId in this.parentIssues) {
+    var parent = deepCopy(this.parentIssues[pId].issue);
 
-            // If first row
-            if (out.length === 0) {
-                // Add header row and parent row
-                var rows = parent.issue.toRowArray();
-                out.push(rows[0]);
-                out.push(rows[1]);
-            } else {
-                // Otherwise just add parent row
-                out.push(parent.issue.toRowArray()[1]);
-            }
+    // Sort children
+    for (var cId in this.parentIssues[pId].children) {
+      var child = this.parentIssues[pId].children[cId];
 
-            // For each child
-            for (var cId in parent.children) {
-                var child = parent.children[cId];
+      parent.children.push(child);
+    }
+    parent.children.sort(Issue.compareIssues);
 
-                out.push(child.toRowArray()[1])
-            }
-        }
+    this.sortedParentIssues.push(parent);
+  }
 
-        return out;
-    };
+  this.sortedParentIssues.sort(Issue.compareIssues);
 
-    return this;
+  /**
+   * Convert IssueTree into Row Array to be returned from spreadsheet function
+   * @returns {Array}
+   */
+  this.toRowArray = function () {
+    var out = [];
+
+    // For each parent
+    for (var i = 0; i < this.sortedParentIssues.length; i++) {
+      var parent = this.sortedParentIssues[i];
+      // Add parent row
+      // If first row
+      if (out.length === 0) {
+        // Add header row and parent row
+        var rows = parent.toRowArray();
+        out.push(rows[0]);
+        out.push(rows[1]);
+      } else {
+        // Otherwise just add parent row
+        out.push(parent.toRowArray()[1]);
+      }
+
+      // For each child
+      for (var cI = 0; cI < parent.children.length; cI++) {
+        out.push(parent.children[cI].toRowArray()[1])
+      }
+    }
+
+    return out;
+  };
+
+  return this;
 }
 
 // Configuration parameters
 var sourceIssuesSheetId = "1RJmN9WXBP-T5djhRad-Xs-nIZPe-QogtMfEuTlY145I";
 
 function taskList() {
-    var sourceIssuesSheet = SpreadsheetApp.openById(sourceIssuesSheetId);
-    var sourceIssuesData = sourceIssuesSheet.getDataRange().getValues();
+  var sourceIssuesSheet = SpreadsheetApp.openById(sourceIssuesSheetId);
+  var sourceIssuesData = sourceIssuesSheet.getDataRange().getValues();
 
-    var issues = Issue.fromRows(sourceIssuesData);
-    var issueTree = IssueTree(issues);
+  var issues = Issue.fromRows(sourceIssuesData);
+  var issueTree = IssueTree(issues);
 
-    return issueTree.toRowArray();
+  return issueTree.toRowArray();
 }
