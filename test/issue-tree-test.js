f85d1952983861684e4d3f4b48206034b017c1a6
Noah Huppert <noahhuppert@gmail.com>
2017-04-24 22:34:36 -0400

    Finished up IssueTree constructor tests

--- test/issue-tree-test.js
+++ test/issue-tree-test.js
@@ -9,10 +9,15 @@ describe("IssueTree", function() {
       return true;
     };
 
-    this.parent1 = {id: 1, parentId: undefined, children: [], hasParent: this.hasParentFalse};
-    this.parent2 = {id: 2, parentId: undefined, children: [], hasParent: this.hasParentFalse};
-    this.childof1 = {id: 3, parentId: 1, children: [], hasParent: this.hasParentTrue};
-    this.childof2 = {id: 4, parentId: 2, children: [], hasParent: this.hasParentTrue};
+    this.parent1 = {id: 1, parentId: undefined, children: [], startDate: new Date(2000), hasParent: this.hasParentFalse};
+    this.parent2 = {id: 2, parentId: undefined, children: [], startDate: new Date(2001), hasParent: this.hasParentFalse};
+    this.childof1 = {id: 3, parentId: 1, children: [], startDate: new Date(1990), hasParent: this.hasParentTrue};
+    this.childof2 = {id: 4, parentId: 2, children: [], startDate: new Date(1990), hasParent: this.hasParentTrue};
+    this.otherChildOf1 = {id: 5, parentId: 1, children: [], startDate: new Date(1991), hasParent: this.hasParentTrue};
+    this.otherChildOf2 = {id: 6, parentId: 2, children: [], startDate: new Date(1991), hasParent: this.hasParentTrue};
+
+    this.allIssues = {1: this.parent1, 2: this.parent2, 3: this.childof1, 4: this.childof2, 5: this.otherChildOf1, 6: this.otherChildOf2};
+    this.allIssuesBackwards = {6: this.otherChildOf2, 5: this.otherChildOf1, 4: this.childof2, 3: this.childof1, 2: this.parent2, 1: this.parent1};
 
     /**
      * Common assert pattern of checking IssueTree.parentIssues contains this.parent1 and this.parent2
@@ -50,10 +55,37 @@ describe("IssueTree", function() {
 
   describe("constructor", function() {
     it("works when parents are mentioned before they are defined", function() {
-      var issues = {4: this.childof2, 3: this.childof1, 2: this.parent2, 1: this.parent1};
-
-      var it = new IssueTree(issues);
+      var it = new IssueTree(this.allIssuesBackwards);
       this.assertContainsParents(it.parentIssues);
+    });
+
+    it("works when parents are mentioned after they are defined", function() {
+      var it = new IssueTree(this.allIssues);
+      this.assertContainsParents(it.parentIssues);
+    });
+
+    it("sorts parent issues", function() {
+      var it = new IssueTree(this.allIssues);
+
+      expect(it.sortedParentIssues[0]).toEqual(this.parent1);
+      expect(it.sortedParentIssues[0].children[0]).toEqual(this.childof1);
+      expect(it.sortedParentIssues[0].children[1]).toEqual(this.otherChildOf1);
+
+      expect(it.sortedParentIssues[1]).toEqual(this.parent2);
+      expect(it.sortedParentIssues[1].children[0]).toEqual(this.childof2);
+      expect(it.sortedParentIssues[1].children[1]).toEqual(this.otherChildOf2)
+    });
+
+    it("generates issuesToRowMap", function() {
+      var it = new IssueTree(this.allIssuesBackwards);
+
+      expect(it.issueToRowMap["1"]).toEqual(2);
+      expect(it.issueToRowMap["3"]).toEqual(3);
+      expect(it.issueToRowMap["5"]).toEqual(4);
+
+      expect(it.issueToRowMap["2"]).toEqual(5);
+      expect(it.issueToRowMap["4"]).toEqual(6);
+      expect(it.issueToRowMap["6"]).toEqual(7);
     });
   });
 });
